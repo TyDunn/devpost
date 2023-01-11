4b9fc3a21b25e09a37ba3bcdaec0fb65895ce8b2
Ty Dunn <ty@tydunn.com>
2023-01-11 07:11:37 +0100

    adding c++ code

---
+++ main.cpp
@@ -0,0 +1,8 @@
+// Code for https://devpost.com/software/cart was written in C++
+
+#include <iostream>
+
+int main() {
+    std::cout << "TODO: Find C++ code for https://devpost.com/software/cart";
+    return 0;
+}
